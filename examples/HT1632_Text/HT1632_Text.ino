b9c8b8f8bd5198343bd9791b913a65526999da67
Benjamin Choy <bench2004@gmail.com>
2020-12-10 16:00:26 +0800

    Update HT1632_Text.ino
    - Change pin for for JY-MCU-3208
    - Updated to renderTaget for V2 library, see upgrade for details

--- examples/HT1632_Text/HT1632_Text.ino
+++ examples/HT1632_Text/HT1632_Text.ino
@@ -7,14 +7,14 @@ int wd;
 char disp [] = "Hello, how are you?";
 
 void setup () {
-  HT1632.begin(12, 10, 9);
+  HT1632.begin(PIN_PB3, PIN_PB4, PIN_PB5);//Change for JY-MCU-3208//
   
   wd = HT1632.getTextWidth(disp, FONT_5X4_END, FONT_5X4_HEIGHT);
 }
 
 void loop () {
   
-  HT1632.drawTarget(BUFFER_BOARD(1));
+  HT1632.renderTarget(0);//Updated for V2 library, see upgrade for details//
   HT1632.clear();
   HT1632.drawText(disp, OUT_SIZE - i, 2, FONT_5X4, FONT_5X4_END, FONT_5X4_HEIGHT);
   HT1632.render();
